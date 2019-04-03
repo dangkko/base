0bee1fd41bee925f2a411aebda41076dca39ece4
dangkko <43430791+dangkko@users.noreply.github.com>
2019-04-03 17:54:06 +0900

    module
    module

--- Base/js/module.js
+++ Base/js/module.js
@@ -51,7 +51,7 @@ $(function() {
 			showOn: "button",
 			buttonText: "달력보기",
 			showAnim:"slideDown",
-			dateFormat:"yy.mm.dd",
+			dateFormat:"yy-mm-dd",
 			changeMonth: true,
 		    changeYear: true,
 		    monthNames: ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"],
