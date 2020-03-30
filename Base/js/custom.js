a4b6d1b6cf795a5073085bcee0650f553f66cd5d
dangkko <43430791+dangkko@users.noreply.github.com>
2020-03-30 17:30:21 +0900

    base

--- Base/js/custom.js
+++ Base/js/custom.js
@@ -11,4 +11,35 @@ $(window).load(function(){
 	} else {
 		//나머지브라우저 컨트롤
 	}
+
+	lnbSetControl();
 });
+
+//lnb load
+function lnbSetControl(){
+	if(!($('.lnb').length > 0))	return;	
+	//gnb 1depth on check
+	if($('#header nav .gnb > li').children('a').hasClass('on')){
+		//1depth on true 2depth save
+		var gnbHtml = $('#header nav .gnb > li > a.on').next('ul').html();
+		//lnb add Html
+		$('.lnb').append(gnbHtml);
+		//nav.lnb ul add html
+		$('.lnb li').wrapAll('<ul></ul>');
+		//lnb link #container add
+		$('.lnb ul > li').each(function(){
+			var lnbLink = $(this).find('a').attr('href');
+			$(this).find('a').attr('href',lnbLink + '#container');
+		});	
+
+		//gnb menu on text
+		var gnbTitle = $('.gnb > li > a.on').text();
+		$('경로').text(gnbTitle);
+		//lnb menu on text
+		var lnbTitle = $('.lnb ul > li > a.on').text();
+		$('경로').text(lnbTitle);
+	}else{
+		//1차가 on이 없는경우 2차가 없음을 간주하고 lnb자체를 숨김처리함 불필요할때 삭제가능
+		$('.lnb').hide();
+	}
+}
